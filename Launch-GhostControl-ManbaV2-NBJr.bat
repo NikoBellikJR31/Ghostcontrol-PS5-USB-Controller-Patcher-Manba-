746baa7bf8e55b728129a84e0cc18d46d522081f
Fsociety <180300849+NikoBellikJR31@users.noreply.github.com>
2026-06-15 18:06:16 +0200

    Delete Launch-GhostControl-ManbaV2-NBJr.bat

--- Launch-GhostControl-ManbaV2-NBJr.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-title GhostControl Manba V2 NBJr
-cd /d "%~dp0"
-echo GhostControl Manba V2 NBJr - cleanup puis payload
-echo.
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Launch-GhostControl-ManbaV2-NBJr.ps1"
-pause
